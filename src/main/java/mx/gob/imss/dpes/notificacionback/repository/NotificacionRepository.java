01d855cd15a179d5ce46c534404133a43cc96f7e
Hector Martinez Espinosa <mtzehector@gmail.com>
2025-10-31 21:36:00 -0600

    Actualizaci├│n inicial del proyecto

--- src/main/java/mx/gob/imss/dpes/notificacionback/repository/NotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.gob.imss.dpes.notificacionback.repository;
-
-import mx.gob.imss.dpes.notificacionback.entity.McltNotificacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface NotificacionRepository extends JpaRepository<McltNotificacion, Long>,
-    JpaSpecificationExecutor<McltNotificacion> {
-}
